1f39e17422fe9864953f2a3f8ff928e950ebd524
Aditya-kumar2005 <nanuadityakumar@gmail.com>
2026-08-17 22:48:21 +0530

    file updated

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,9 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  turbopack: {
-    root: path.resolve(__dirname, "../../"), // Points to monorepo root (C:\Users\nanua\New-project)
-  },
+  // turbopack: {
+  //   root: path.resolve(__dirname, "../../"), // Points to monorepo root (C:\Users\nanua\New-project)
+  // },
 };
 
 export default nextConfig;
